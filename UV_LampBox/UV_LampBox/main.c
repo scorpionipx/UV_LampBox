ebf2b50d998e12680298cbd22a3050b092801bef
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-04-14 22:48:19 +0300

    Project files

--- UV_LampBox/UV_LampBox/main.c
+++ UV_LampBox/UV_LampBox/main.c
@@ -5,12 +5,19 @@
  * Author : ScorpionIPX
  */ 
 
+#define F_CPU 1000000UL
+
 #include <avr/io.h>
 
 
 int main(void)
 {
     /* Replace with your application code */
+	
+	DDRD = 0xFF;
+	PORTD &= 1 << 7;
+	PORTD &= 1 << 6;
+	
     while (1) 
     {
     }
