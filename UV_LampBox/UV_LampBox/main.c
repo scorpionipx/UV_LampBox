933e4874d7ee18a7d1466d9f3d5d987b21398de7
DESKTOP-G3E1MO0\ScorpionIPX <scorpionipx@gmail.com>
2017-04-14 16:41:04 +0300

    Atmel Studio project files

---
+++ UV_LampBox/UV_LampBox/main.c
@@ -0,0 +1,18 @@
+/*
+ * UV_LampBox.c
+ *
+ * Created: 4/14/2017 4:40:44 PM
+ * Author : ScorpionIPX
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
